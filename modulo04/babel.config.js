8e2eb3387b8d36e28d76932a62e0245733018a9f
Edgard Levy <edgardlevy.apple@gmail.com>
2019-10-30 13:54:28 -0200

    - inicio da criacao do projeto com create react app

--- modulo04/babel.config.js
+++ modulo04/babel.config.js
@@ -1,6 +1,7 @@
 /**
  * @babel/preset-env = responsavel por alterar as funcoes do javascript que o browser nao entende. ex: import, export, class functions
  * @babel/preset-react = faz o mesmo do preset-env mas focado no react, tipo jsx
+ * @babel/plugin-proposal-class-properties
  */
 module.exports = {
   presets: [
