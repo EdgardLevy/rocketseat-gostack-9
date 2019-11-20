5e89388ed4ae78302ef608cdc1f98f85fceb2648
Edgard Levy <edgardlevy.apple@gmail.com>
2019-11-20 18:18:11 -0200

    - implementacao da janela de profile  - implementacao o logout da aplicacao

--- modulo09/src/components/Header/index.js
+++ modulo09/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import {useSelector} from 'react-redux';
 import Notifications from '~/components/Notifications';
 
 import logo from '~/assets/logo-purple.svg';
@@ -8,6 +8,7 @@ import logo from '~/assets/logo-purple.svg';
 import {Container, Content, Profile} from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -19,11 +20,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Edgard Levy</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Profile"
             />
           </Profile>
--- modulo09/src/pages/Profile/index.js
+++ modulo09/src/pages/Profile/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {Form, Input} from '@rocketseat/unform';
+import {updateProfileRequest} from '~/store/modules/user/actions';
+import {signOut} from '~/store/modules/auth/actions';
+import {Container} from './styles';
 
-// import { Container } from './styles';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+  function handleSubmit(data) {
+    // console.tron.log(data);
+    dispatch(updateProfileRequest(data));
+  }
+  function handleLogout() {
+    dispatch(signOut());
+  }
   return (
-    <div />
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Sua nova senha" />
+        <Input
+          type="password"
+          name="confirmePassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button" onClick={handleLogout}>
+        Sair do Gobarber
+      </button>
+    </Container>
   );
 }
