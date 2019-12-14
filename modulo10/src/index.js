dfb66f5a810962145ffb6f1d622e136304896489
Edgard Levy <edgardlevy@gmail.com>
2019-12-13 23:46:06 -0300

    modulo 10  - rota inicial

--- modulo10/src/index.js
+++ modulo10/src/index.js
@@ -6,15 +6,16 @@ import {PersistGate} from 'redux-persist/integration/react';
 // import { Container } from './styles';
 import './config/ReactotronConfig';
 import {persistor, store} from './store';
-import Routes from './routes';
 
-export default function App() {
+import App from './App';
+
+export default function Index() {
   return (
     <>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-          <Routes />
+          <App />
         </PersistGate>
       </Provider>
     </>
