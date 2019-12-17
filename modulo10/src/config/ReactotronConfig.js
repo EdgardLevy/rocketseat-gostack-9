010192a7b8f2340b5e6b30c54a373afb7d233d07
Edgard Levy <edgardlevy.apple@gmail.com>
2019-12-17 00:09:10 -0200

    - estilizacao do dashboard

--- modulo10/src/config/ReactotronConfig.js
+++ modulo10/src/config/ReactotronConfig.js
@@ -1,11 +1,11 @@
 import Reactotron from 'reactotron-react-native';
-import {reactotronRedux} from 'reactotron-redux';
+import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 import AsyncStorage from '@react-native-community/async-storage';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-    .configure({host: '192.168.1.10'})
+    .configure({ host: '192.168.1.9' })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
--- modulo10/src/pages/Dashboard/index.js
+++ modulo10/src/pages/Dashboard/index.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
 import { Container, Title, List } from './styles';
 import Appointment from '~/components/Appointment';
+import api from '~/services/api';
 
-const data = [1, 2, 3, 4, 5];
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+      console.tron.log(response);
+      setAppointments(response.data);
+    }
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    // console.tron.log(id);
+    const response = await api.delete(`appointments/${id}`);
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? { ...appointment, canceled_at: response.data.canceled_at }
+          : appointment,
+      ),
+    );
+  }
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
         <List
-          data={data}
-          renderItem={({ item }) => <Appointment data={item} />}
-          keyExtractor={item => item}
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
         />
       </Container>
     </Background>
