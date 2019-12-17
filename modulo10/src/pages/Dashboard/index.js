f829499470ba5f94a22cd7dce87d55f60b5f424e
Edgard Levy <edgardlevy.apple@gmail.com>
2019-12-16 23:26:50 -0200

    - estilizacao dos agendamentos

--- modulo10/src/pages/Dashboard/index.js
+++ modulo10/src/pages/Dashboard/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '~/components/Background';
+import { Container, Title, List } from './styles';
+import Appointment from '~/components/Appointment';
 
-// import { Container } from './styles';
-
+const data = [1, 2, 3, 4, 5];
 export default function Dashboard() {
-  return <View />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={data}
+          renderItem={({ item }) => <Appointment data={item} />}
+          keyExtractor={item => item}
+        />
+      </Container>
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
